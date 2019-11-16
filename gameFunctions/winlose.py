5e92d255766cfcc2d27e6ec6335a41446c7905a2
Harshpreet Kaur <h_harshpreetkaur@F1000-mac-07.fc.ca>
2019-11-15 21:05:00 -0500

    changes

--- gameFunctions/winlose.py
+++ gameFunctions/winlose.py
@@ -1,7 +1,9 @@
 from random import randint
+from gameFunctions import gameVars 
+
 # define a python function that takes an argument
-def winorlose(status): 
-	# status will be either won or lost - you're passing this in as an argument
+def winorlose(status):
+	#status will be either won or lost - you're passng this in as an argument	
 	print("called win or lose")
 	print("************************")
 
@@ -16,16 +18,13 @@ def winorlose(status):
 
 	elif (choice is "Y") or (choice is "y"):
 		# reset the game so that we can start all over again
-		# this will break, currently - we will fix this next class
-		player_lives = 1
-		computer_lives = 1
-		player = False
-		computer = choices[randint(0,2)]
+		gameVars.player_lives = 1
+		gameVars.computer_lives = 1
+		gameVars.total_lives = 1
+		gameVars.player = False
+		gameVars.computer = gameVars.choices[randint(0,2)]
 	
 	else:
-		# not a y or n, so make the user pick a valid choice
-		print("make a valid choice, Y or N")
-		# this is recursion - call a function
-		# from inside itself. Basically just re-up the choice
-		# and force the user to pick yes or no (y or n)
+		 # use recursion to call winorlose again until we get the right 
+		 # recursion is just a fancy way to describe calling a function from within 
 		winorlose(status)
